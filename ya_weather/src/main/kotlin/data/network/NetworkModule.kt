088ff11b66bb1a1d98ae1f25d5e9ef6190af66a7
Odiljon Bobosodikov <odilbobosodikov2002@gmail.com>
2024-11-30 17:01:37 +0500

    commit: Updated Network module

---
+++ ya_weather/src/main/kotlin/data/network/NetworkModule.kt
@@ -0,0 +1,28 @@
+package data.network
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@Singleton
+class NetworkProvider {
+  private val json = Json { ignoreUnknownKeys = true }
+
+  @Provides
+  fun providesOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+  @Provides
+  fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit =
+    Retrofit.Builder()
+      .baseUrl("https://api.openweathermap.org/")
+      .client(okHttpClient)
+      .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+      .build()
+}
---
+++ ya_weather/src/main/kotlin/data/network/WeatherApi.kt
@@ -0,0 +1,14 @@
+package data.network
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+  @GET("data/2.5/weather")
+  suspend fun current(
+    @Query("lat") lat: String,
+    @Query("lon") lon: String,
+    @Query("appid") appid: String
+  ): Response<CoordinatesResponse>
+}
