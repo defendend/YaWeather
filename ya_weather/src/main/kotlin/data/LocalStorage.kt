5c979115949230209bfef71a7374d2c790881c37
Odiljon Bobosodikov <odilbobosodikov2002@gmail.com>
2024-11-30 16:06:18 +0500

    commit: created basic Dagger infrastructure

---
+++ ya_weather/src/main/kotlin/data/LocalStorage.kt
@@ -0,0 +1,10 @@
+package data
+
+import android.content.Context
+import javax.inject.Inject
+
+
+class LocalStorage @Inject constructor(context: Context) : SharedPreference(context) {
+    var isDark by booleans(false)
+    var token by strings("Nellas")
+}
