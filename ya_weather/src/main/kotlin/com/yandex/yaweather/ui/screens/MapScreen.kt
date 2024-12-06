a54fab49fe1f7791b52912dff7172ccfefb84d2f
User <abbosbeknormuratov2003@gmail.com>
2024-12-06 22:00:43 +0500

    GeoLocation added to mapScreen

--- ya_weather/src/main/kotlin/com/yandex/yaweather/ui/screens/MapScreen.kt
+++ ya_weather/src/main/kotlin/com/yandex/yaweather/ui/screens/MapScreen.kt
@@ -24,14 +24,19 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.yandex.yaweather.handler.MapScreenAction
-import com.yandex.yaweather.viewModel.WeatherUiState
+import com.yandex.yaweather.viewModel.MapUIState
+
 
 @Composable
 fun MapScreen(
-  uiState: WeatherUiState,
+  uiState: MapUIState,
   action: (MapScreenAction) -> Unit
 ) {
-  val toshekent = LatLng(41.2995, 69.2401)
+  val toshekent = uiState.markerPosition?.let {
+    it.lat?.let { it1 -> it.lon?.let { it2 -> LatLng(it1, it2) } }
+
+  } ?: LatLng(41.311081, 69.240562)
+
   val cameraPositionState = rememberCameraPositionState {
     position = CameraPosition.fromLatLngZoom(toshekent, 10f)
   }
@@ -39,182 +44,218 @@ fun MapScreen(
   var isNightMode by remember { mutableStateOf(false) }
   val context = LocalContext.current
 
+
   val nightModeStyle = """
-        [
+    [
+      {
+        "elementType": "geometry",
+        "stylers": [
           {
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#242f3e"
-              }
-            ]
-          },
-          {
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#746855"
-              }
-            ]
-          },
-          {
-            "elementType": "labels.text.stroke",
-            "stylers": [
-              {
-                "color": "#242f3e"
-              }
-            ]
-          },
-          {
-            "featureType": "administrative.locality",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#d59563"
-              }
-            ]
-          },
-          {
-            "featureType": "poi",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#d59563"
-              }
-            ]
-          },
-          {
-            "featureType": "poi.park",
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#263c3f"
-              }
-            ]
-          },
-          {
-            "featureType": "poi.park",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#6b9a76"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#38414e"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "geometry.stroke",
-            "stylers": [
-              {
-                "color": "#212a37"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#9ca5b3"
-              }
-            ]
-          },
-          {
-            "featureType": "road.highway",
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#746855"
-              }
-            ]
-          },
-          {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [
-              {
-                "color": "#1f2835"
-              }
-            ]
-          },
-          {
-            "featureType": "road.highway",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#f3d19c"
-              }
-            ]
-          },
-          {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#2f3948"
-              }
-            ]
-          },
-          {
-            "featureType": "transit.station",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#d59563"
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#17263c"
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#515c6d"
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "labels.text.stroke",
-            "stylers": [
-              {
-                "color": "#17263c"
-              }
-            ]
+            "color": "#242f3e"
           }
         ]
-    """.trimIndent()
-
-
-
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#746855"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#242f3e"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#263c3f"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#6b9a76"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#38414e"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#212a37"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9ca5b3"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#746855"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#1f2835"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#f3d19c"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#2f3948"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#17263c"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#515c6d"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#17263c"
+          }
+        ]
+      }
+    ]
+""".trimIndent()
+  val nightModeStyleOptions = remember {
+    MapStyleOptions(nightModeStyle)
+  }
   Column(modifier = Modifier.fillMaxSize()) {
     Button(
-      onClick = { isNightMode = !isNightMode },
+      onClick = { isNightMode = !isNightMode
+        Log.d("MapMode", "Mode Changed: ${if (isNightMode) "Night" else "Day"}")
+      },
       modifier = Modifier
         .align(Alignment.CenterHorizontally)
         .padding(8.dp)
     ) {
       Text(text = if (isNightMode) "Day Mode" else "Night Mode")
     }
-
     GoogleMap(
       modifier = Modifier.fillMaxSize(),
       cameraPositionState = cameraPositionState,
@@ -224,19 +265,20 @@ fun MapScreen(
         action(MapScreenAction.UpdateMarkerPositionAction(latLng))
       },
       properties = MapProperties(
-        mapStyleOptions = if (isNightMode) {
-          MapStyleOptions(nightModeStyle)
-        } else null
+        mapStyleOptions = if (isNightMode) nightModeStyleOptions else null
+
       )
     ) {
+
+
       Log.d("MapMode", "Night Mode Active: $isNightMode")
       markerPosition?.let { position ->
         Marker(
           state = MarkerState(position = position),
           title = "Temperature: ${uiState.temperature}°C",
+
         )
       }
     }
   }
-
 }
