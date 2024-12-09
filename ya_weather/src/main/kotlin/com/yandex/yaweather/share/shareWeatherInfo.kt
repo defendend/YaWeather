167468543e765a980e917b346e62d0ca508dc064
User <abbosbeknormuratov2003@gmail.com>
2024-12-09 15:20:49 +0500

    shareee done

--- ya_weather/src/main/kotlin/com/yandex/yaweather/share/shareWeatherInfo.kt
+++ ya_weather/src/main/kotlin/com/yandex/yaweather/share/shareWeatherInfo.kt
@@ -2,16 +2,15 @@ package com.yandex.yaweather.share
 
 import android.content.Context
 import android.content.Intent
-import com.yandex.yaweather.viewModel.WeatherUiState
 
 fun shareWeatherInfo(context: Context, weatherInfo: String){
     val shareIntent = Intent(Intent.ACTION_SEND).apply {
       type="text/plain"
-      putExtra(Intent.EXTRA_SUBJECT, "Об-ҳаво маълумотлари") // Sarlavha
+      putExtra(Intent.EXTRA_SUBJECT, "Погодная информация")
       putExtra(Intent.EXTRA_TEXT, weatherInfo)
 
     }
-  context.startActivity(Intent.createChooser(shareIntent,"Ulashish"))
+  context.startActivity(Intent.createChooser(shareIntent,"отправлять"))
 
 
 }
